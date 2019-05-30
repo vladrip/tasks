f52ffde268e237110962e5def1912221def0644a
vladrip <51162854+vladrip@users.noreply.github.com>
2019-05-30 18:36:40 +0300

    Add files via upload

---
+++ Shuffle.js
@@ -0,0 +1,6 @@
+function shuffleArray(names) {
+var newArray = [];
+names.sort() = Math.floor(Math.random() * names.length);
+var neww = newArray.concat(names);
+return newArray}
+shuffleArray(["Andriy", "Mary", "Vasyan", "Petrol"])
