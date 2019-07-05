dbcb68fd1d6197aec321bcef3575c2db6b567395
vladrip <51162854+vladrip@users.noreply.github.com>
2019-07-05 11:54:53 +0300

    Add files via upload

---
+++ oddEven.js
@@ -0,0 +1,16 @@
+function countOddEvenNumbers(n) {
+    var result = {odd: 0, even: 0};
+    if (typeof n === 'number') {
+       n = n.toString();
+    }
+    var arr = Array.from(n);
+    arr.forEach(function(oddEven) {
+      if (oddEven % 2 === 0) {
+        result.odd += 1;
+      } else {
+        result.even += 1;
+      }
+    });
+  
+    return result;
+  }
