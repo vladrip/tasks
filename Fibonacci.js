e700f39190f02079614f7b03c7458a455ac98ff6
vladrip <51162854+vladrip@users.noreply.github.com>
2019-07-06 10:00:54 +0300

    Update Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,5 +1,13 @@
 function FibonacciSeq(n) {
 var fibo = [1,1]
+switch (n) {
+    case 0:
+        return null
+        break;
+    case 1:
+        fibo.pop(); return fibo;
+        break;
+    default:    
 for(var i=2; i<n; i++) {
 fibo[i] = fibo[i-1] + fibo[i-2] }
-return fibo }
+return fibo }}
